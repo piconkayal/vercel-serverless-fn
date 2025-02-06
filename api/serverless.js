371cd15ac4f1a2574842356194590abcee5ae3df
Picon Kayal <piconn.js@gmail.com>
2025-02-07 00:58:26 +0530

    awdaw

---
+++ api/serverless.js
@@ -0,0 +1,17 @@
+const Fastify = require("fastify")
+
+// Create fastify instance outside the handler
+const app = Fastify({
+  logger: true,
+});
+
+// Register your plugins
+await app.register(import("../app/index.js"));
+
+// Export the serverless function
+module.exports = async function handler(req, res) {
+  await app.ready();
+  app.server.emit('request', req, res);
+  
+  return app;
+}
